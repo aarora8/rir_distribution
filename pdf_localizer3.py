6909df11ea0b2fc07c12be4cc74a56c5d18783ef
Ashish Arora <ashisharora88888@gmail.com>
2019-06-12 16:31:28 -0400

    Add files via upload

--- pdf_localizer3.py
+++ pdf_localizer3.py
@@ -15,18 +15,13 @@ xml_path = '/Users/ashisharora/text2.xml'
 doc = minidom.parse(xml_path)
 page1 = doc.getElementsByTagName('page')
 count = 0
-padding_x = 10
-padding_y = 2
-tolerance = 5
-x_mindiff = 30
-y_mindiff = 5
+
 xMin_dict = dict()
 xMax_dict = dict()
 yMin_dict = dict()
 yMax_dict = dict()
 width_list = list()
 height_list = list()
-box_dict = dict()
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
 
@@ -45,26 +40,16 @@ for page in page1:
     yMin_dict[pageid] = list()
     xMax_dict[pageid] = list()
     yMax_dict[pageid] = list()
-    box_dict[pageid] = list()
     for word in word1:
         wordid += 1
         xMin = float(word.getAttribute('xMin'))
         yMin = float(word.getAttribute('yMin'))
         xMax = float(word.getAttribute('xMax'))
         yMax = float(word.getAttribute('yMax'))
-        a = [xMin, yMin, xMax, yMax]
-        box_dict[pageid].append(a)
         xMin_dict[pageid].append(xMin)
         yMin_dict[pageid].append(yMin)
         xMax_dict[pageid].append(xMax)
         yMax_dict[pageid].append(yMax)
-
-
-for pageid in range(len(width_list)):
-    if len(box_dict[pageid]) == 0:
-        print(pageid)
-        continue
-    box_dict[pageid].sort(key=lambda x: x[1])
 
 
 def get_linedict_from_pagelist(xMin_page_list, yMin_page_list, xMax_page_list, yMax_page_list):
@@ -94,35 +79,6 @@ def get_linedict_from_pagelist(xMin_page_list, yMin_page_list, xMax_page_list, y
     return xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict
 
 
-def get_linedict_from_boxpagelist(box_page_list):
-    xMin_line_dict = dict()
-    xMax_line_dict = dict()
-    yMin_line_dict = dict()
-    yMax_line_dict = dict()
-    xMin_line_dict[0] = list()
-    xMax_line_dict[0] = list()
-    yMin_line_dict[0] = list()
-    yMax_line_dict[0] = list()
-
-    prev_yMin = box_page_list[0][1]
-    line_id = 0
-    for i in range(len(box_page_list)):
-        curr_yMin = box_page_list[i][1]
-        # if prev_yMin != curr_yMin:
-        if (curr_yMin - prev_yMin) > tolerance:
-            prev_yMin = curr_yMin
-            line_id += 1
-            xMin_line_dict[line_id] = list()
-            xMax_line_dict[line_id] = list()
-            yMin_line_dict[line_id] = list()
-            yMax_line_dict[line_id] = list()
-        xMin_line_dict[line_id].append(box_page_list[i][0])
-        yMin_line_dict[line_id].append(box_page_list[i][1])
-        xMax_line_dict[line_id].append(box_page_list[i][2])
-        yMax_line_dict[line_id].append(box_page_list[i][3])
-    return xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict
-
-
 def get_linelist_from_linedict(xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict):
     xMin_line_list = list()
     xMax_line_list = list()
@@ -133,10 +89,10 @@ def get_linelist_from_linedict(xMin_line_dict, yMin_line_dict, xMax_line_dict, y
         xMax_list = xMax_line_dict[lineid]
         yMin_list = yMin_line_dict[lineid]
         yMax_list = yMax_line_dict[lineid]
-        xMin = min(xMin_list)
-        xMax = max(xMax_list)
-        yMin = min(yMin_list)
-        yMax = max(yMax_list)
+        xMin = xMin_list[0]
+        xMax = xMax_list[-1]
+        yMin = yMin_list[0]
+        yMax = yMax_list[0]
         xMin_line_list.append(xMin)
         xMax_line_list.append(xMax)
         yMin_line_list.append(yMin)
@@ -147,10 +103,10 @@ def get_linelist_from_linedict(xMin_line_dict, yMin_line_dict, xMax_line_dict, y
 filtered_count = 0
 filtered_count1 = 0
 doc = fitz.Document('/Users/ashisharora/Desktop/A11_MissionReport.pdf')
-img_path = '/Users/ashisharora/Desktop/dct/pdf_imgs5/'
-for pageid in range(11, len(doc)):
+img_path = '/Users/ashisharora/Desktop/dct/pdf_imgs2/'
+for pageid in range(len(doc)):
     full_dir_path = img_path + str(pageid)
-    os.mkdir(full_dir_path)
+    # os.mkdir(full_dir_path)
     page = doc.loadPage(pageid)
     pix = page.getPixmap()
     mode = "RGBA" if pix.alpha else "RGB"
@@ -159,14 +115,11 @@ for pageid in range(11, len(doc)):
     yMin_page_list = yMin_dict[pageid]
     xMax_page_list = xMax_dict[pageid]
     yMax_page_list = yMax_dict[pageid]
-    box_page_list = box_dict[pageid]
-
     if len(yMax_page_list) == 0:
         print(pageid)
         continue
     print(pageid)
-    # xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict = get_linedict_from_pagelist(xMin_page_list, yMin_page_list, xMax_page_list, yMax_page_list)
-    xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict = get_linedict_from_boxpagelist(box_page_list)
+    xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict = get_linedict_from_pagelist(xMin_page_list, yMin_page_list, xMax_page_list, yMax_page_list)
     xMin_line_list, yMin_line_list, xMax_line_list, yMax_line_list = get_linelist_from_linedict(xMin_line_dict, yMin_line_dict, xMax_line_dict, yMax_line_dict)
     rect_list = list()
     for lineid in range(len(xMin_line_list)):
@@ -174,28 +127,27 @@ for pageid in range(11, len(doc)):
         xMax = xMax_line_list[lineid]
         yMin = yMin_line_list[lineid]
         yMax = yMax_line_list[lineid]
+        rect1 = patches.Rectangle((xMin, yMin), xMax - xMin, yMax - yMin, linewidth=1, edgecolor='r',
+                              facecolor='none')
+        box = (xMin, yMin, xMax, yMax)
         if (xMax <= xMin) or (yMax <= yMin):
             print(lineid)
             filtered_count += 1
             continue
-        if (xMax - xMin) <=x_mindiff or (yMax - yMin) <= y_mindiff:
+        if (xMax - xMin) <=30 or (yMax - yMin) <= 2:
             print(lineid)
             filtered_count1 += 1
             continue
-        # rect1 = patches.Rectangle((xMin - padding_x, yMin - padding_y), xMax - xMin + 2*padding_x, yMax - yMin + 2*padding_y, linewidth=1, edgecolor='r',
-        #                           facecolor='none')
-        # ax.add_patch(rect1)
-        box = (xMin - padding_x, yMin - padding_y, xMax + padding_x, yMax + padding_y)
+        ax.add_patch(rect1)
         region_initial = img.crop(box)
-        
         img_name = str(pageid) + '_' + str(lineid) +'.png'
         full_img_path = full_dir_path + '/' + img_name
-        region_initial.save(full_img_path, quality=100)
-    # ax.imshow(img)
-    # img_name = str(pageid) + '.png'
-    # full_img_path = img_path + img_name
-    # fig.savefig(full_img_path, dpi=600)
-    # ax.clear()
+        # region_initial.save(full_img_path, quality=100)
+    ax.imshow(img)
+    img_name = str(pageid) + '.png'
+    full_img_path = img_path + img_name
+    fig.savefig(full_img_path, dpi=600)
+    ax.clear()
 
 print(filtered_count)
 print(filtered_count1)
