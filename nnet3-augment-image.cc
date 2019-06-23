c94ea53cde81eda48407a1ad0583f7f2c09b4c10
Ashish Arora <ashisharora88888@gmail.com>
2019-06-22 22:22:50 -0400

    Update nnet3-augment-image.cc

--- nnet3-augment-image.cc
+++ nnet3-augment-image.cc
@@ -64,34 +64,64 @@ void compute_dct_matrix(float cepstral_lifter, Matrix<BaseFloat> *dct_matix) {
 }
 
 void freq_mask(Matrix<BaseFloat> *inp_mfcc) {
-  int32 feats_width = inp_mfcc->NumRows(),
+  int32 num_frames = inp_mfcc->NumRows(),
       num_mel = inp_mfcc->NumCols();
 
   Matrix<BaseFloat> dct_matix(num_mel, num_mel), idct_matix(num_mel, num_mel);
+  Matrix<BaseFloat> fbank(num_frames, num_mel);
+  Vector<BaseFloat> unit_vec(num_frames);
+  Vector<BaseFloat> avg_vec(num_mel);
   compute_dct_matrix(22.0, &dct_matix);
   compute_idct_matrix(22.0, &idct_matix);
-
-  int32 srand_seed = 0;
-  int32 F = 27;
-  srand(srand_seed);
-  int32 f = RandInt(0, F);
+ 
+  int32 F = 13;
+  int32 f = RandInt(1, F);
   int32 f_zero = RandInt(0, num_mel-f);
-  Matrix<BaseFloat> fbank(feats_width, num_mel);
-  // get mean for f_zero to f_zero + f
-  Vector<BaseFloat> unit_vect(feats_width);
-  unit_vect.Set(1.0);
-  Vector<BaseFloat> avg_vect(num_mel);
-  avg_vect.AddMatVec(1.0/feats_width, *inp_mfcc, kTrans, unit_vect, 0.0);
   fbank.AddMatMat(1.0, *inp_mfcc, kNoTrans,
                           idct_matix, kTrans, 0.0);
-  for (int32 i = f_zero; i < f_zero+f; i++)
-    for (int32 j = 0; j < feats_width; j++)
-          fbank(i,j) = avg_vect(i);
+  // get mean for each coefficient
+  unit_vec.Set(1.0);
+  avg_vec.AddMatVec(1.0/num_frames, fbank, kTrans, unit_vec, 0.0);
+
+  // mask utterance
+  for (int32 i = f_zero; i < f_zero + f; i++)
+    for (int32 j = 0; j < num_frames; j++)
+          fbank(j,i) = avg_vec(i);
 
   (*inp_mfcc).AddMatMat(1.0, fbank, kNoTrans,
                           dct_matix, kTrans, 0.0);
+}
 
-  //compare input mfcc and mfcc
+int time_mask(Matrix<BaseFloat> *inp_mfcc) {
+  int32 num_frames = inp_mfcc->NumRows(),
+      num_mel = inp_mfcc->NumCols();
+
+  Matrix<BaseFloat> fbank(num_frames, num_mel);
+  int32 F = 15;
+  int32 f = RandInt(1, F);
+  if (num_frames - f <= 0)
+    return 1;
+
+  int32 f_zero = RandInt(0, num_frames-f);
+  int32 f_one;
+  if ((f_zero - f) < 0 && (num_frames - f) < (f_zero + f))
+    return 1;
+
+  if ( f_zero - f > num_frames - f_zero - 2*f) {
+    f_one = RandInt(0, (f_zero - f));
+  }
+  else {
+    f_one = RandInt((f_zero + f), (num_frames - f));
+  }
+  SubMatrix<BaseFloat> segment_zero(*inp_mfcc, f_zero, f, 0, num_mel);
+  SubMatrix<BaseFloat> segment_one(*inp_mfcc, f_one, f, 0, num_mel);
+  Matrix<BaseFloat> copy_zero(f,num_mel), copy_one(f,num_mel);
+
+  copy_zero.CopyFromMat((*inp_mfcc).Range(f_zero, f, 0, num_mel), kNoTrans);
+  copy_one.CopyFromMat((*inp_mfcc).Range(f_one, f, 0, num_mel), kNoTrans);
+  segment_zero.CopyFromMat(copy_one);
+  segment_one.CopyFromMat(copy_zero);
+  return 0;
 }
 
 void PerturbImageInNnetExample(NnetChainExample *eg) {
@@ -123,21 +153,22 @@ int main(int argc, char *argv[]) {
         "modifying of each image, randomly chosen according to configuration\n"
         "parameters).\n"
         "E.g.:\n"
-        "  nnet3-egs-augment-image ark:- ark:-\n"
+        "  nnet3-augment-image ark:- ark:-\n"
         "\n"
         "Requires that each eg contain a NnetIo object 'input', with successive\n"
-        "'t' values representing different x offsets , and the feature dimension\n"
-        "representing the y offset and the channel (color), with the channel\n"
-        "varying the fastest.\n"
+        "'t' values representing time , and the feature dimension\n"
+        "representing the mfcc features.\n"
         "See also: nnet3-copy-egs\n";
 
     int32 srand_seed = 0;
+    bool perform_fmask = false;
     ParseOptions po(usage);
-    po.Register("srand", &srand_seed, "Seed for the random number generator");
+    po.Register("srand", &srand_seed, "Seed for random number generator ");
+    po.Register("fmask", &perform_fmask, "Write output in binary mode");
+
     po.Read(argc, argv);
     srand(srand_seed);
-
-    if (po.NumArgs() < 2) {
+    if (po.NumArgs() != 2) {
       po.PrintUsage();
       exit(1);
     }
@@ -149,13 +180,18 @@ int main(int argc, char *argv[]) {
     NnetChainExampleWriter example_writer(examples_wspecifier);
 
     int64 num_done = 0;
-    for (; !example_reader.Done(); example_reader.Next(), num_done++) {
+    for (; !example_reader.Done(); example_reader.Next()) {
       std::string key = example_reader.Key();
       NnetChainExample eg(example_reader.Value());
-      PerturbImageInNnetExample(&eg);
-      //example_writer.Write(key, eg);
+
+      if(perform_fmask) {
+        PerturbImageInNnetExample(&eg);
+        num_done++;
+      }
+
+      example_writer.Write(key, eg);
     }
-    KALDI_LOG << "Perturbed neural-network training images.";
+    KALDI_LOG << "Performed fmask or tmask on neural-network training images for " << num_done << " images ";
     return (num_done == 0 ? 1 : 0);
   } catch(const std::exception &e) {
     return -1;
