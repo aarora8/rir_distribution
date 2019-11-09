294e9e6636e3ded10e323262b53fb1b8e9d9c0be
Ashish Arora <ashisharora88888@gmail.com>
2019-11-09 12:28:41 -0500

    Update get_ref_1.py

--- get_ref_1.py
+++ get_ref_1.py
@@ -18,25 +18,43 @@ def get_args():
 
 
 def main():
-  args = get_args()
+    args = get_args()
+    sessionid_speakerid_dict= {}
+    spkrid_mapping = {}
+    for line in open(args.text_path):
+        parts = line.strip().split()
+        uttid_id = parts[0]
+        speakerid = uttid_id.strip().split('_')[0]
+        sessionid = uttid_id.strip().split('_')[1]
+        sessionid_speakerid = sessionid + '_' + speakerid
+        if sessionid_speakerid not in list(sessionid_speakerid_dict.keys()):
+            sessionid_speakerid_dict[sessionid_speakerid]=list()
+        sessionid_speakerid_dict[sessionid_speakerid].append(line)
 
-  # find all recording ids present in the text file
-  # split text based on recording_id
-  #P05_S02_U02_KITCHEN.ENH-0266866-0266958
-  sessionid_speakerid_dict= {}
-  spkrid_dict = {}
-  for line in open(args.text_path):
-    parts = line.strip().split()
-    uttid_id = parts[0]
-    speakerid = uttid_id.strip().split('_')[0]
-    sessionid = uttid_id.strip().split('_')[1]
-    sessionid_speakerid = sessionid + '_' + speakerid
-    if sessionid_speakerid not in list(sessionid_speakerid_dict.keys()):
-      sessionid_speakerid_dict[sessionid_speakerid]=list()
-    sessionid_speakerid_dict[sessionid_speakerid].append(line)
+    spkr_num = 1
+    prev_sessionid = ''
+    for sessionid_speakerid in sorted(sessionid_speakerid_dict.keys()):
+        spkr_id = sessionid_speakerid.strip().split('_')[1]
+        curr_sessionid = sessionid_speakerid.strip().split('_')[0]
+        if prev_sessionid != curr_sessionid:
+            prev_sessionid = curr_sessionid
+            spkr_num = 1
+        if spkr_id not in list(spkrid_mapping.keys()):
+            spkrid_mapping[spkr_id] = spkr_num
+            spkr_num += 1
+
+    for sessionid_speakerid in sorted(sessionid_speakerid_dict.keys()):
+        ref_file = args.output_path + '/ref_' + sessionid_speakerid.split('_')[0] + '_' + str(
+            spkrid_mapping[sessionid_speakerid.split('_')[1]])
+        ref_writer = open(ref_file, 'w')
+        text = sessionid_speakerid_dict[sessionid_speakerid]
+        for line in text:
+            ref_writer.write(line)
+        ref_writer.close()
 
 
-  print(sorted(sessionid_speakerid_dict.keys()))
+if __name__ == '__main__':
+    main()
 
   spkrid_mapping = {}
   spkr_num = 1
